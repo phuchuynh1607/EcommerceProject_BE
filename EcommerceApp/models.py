bfb23bde45b8ab025e70c6f60e6d2c4ca00f4cf1
Phuc Huynh <huynhdaiphuc10a9z.com>
2025-12-23 23:28:38 +0700

    First setup FastAPI with authentication user,admin

---
+++ EcommerceApp/models.py
@@ -0,0 +1,45 @@
+from .database import  Base
+from sqlalchemy import Boolean,Column, Integer, String, ForeignKey,Double
+
+
+class Users(Base):
+    __tablename__='users'
+
+    id=Column(Integer,primary_key=True,index=True)
+    email=Column(String,unique=True)
+    username=Column(String,unique=True)
+    first_name=Column(String)
+    last_name=Column(String)
+    hashed_password=Column(String)
+    role=Column(String)
+    phone_number=Column(String)
+    is_active = Column(Boolean, default=True)
+
+
+
+class Products(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer,primary_key=True,index=True)
+    title=Column(String)
+    description=Column(String)
+    price=Column(Double)
+    stock=Column(Integer)
+
+class Carts(Base):
+    __tablename__ ='carts'
+
+    id = Column(Integer,primary_key=True,index=True)
+    user_id = Column(Integer,ForeignKey("users.id"))
+    product_id = Column(Integer,ForeignKey("products.id"))
+    quantity=Column(Integer,default =1)
+
+class Orders(Base):
+    __tablename__='orders'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer,ForeignKey("users.id"))
+    total_price=Column(Double)
+    status=Column(String,default="pending")
+
+
