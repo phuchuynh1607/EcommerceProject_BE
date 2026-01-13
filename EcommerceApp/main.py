06b1ae03525a48e253008040deb22504896c7c9c
phuchuynh1607 <huynhdaiphuc10a9z@gmail.com>
2026-01-13 13:30:10 +0700

    Open CORS for connection

--- EcommerceApp/main.py
+++ EcommerceApp/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from EcommerceApp.database import Base,engine
 from EcommerceApp.routers import auth,admin,users,products,carts,orders
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], # Cho phép cổng của Frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app =FastAPI()
 
 Base.metadata.create_all(bind=engine)
